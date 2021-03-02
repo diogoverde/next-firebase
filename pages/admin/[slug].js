ae451bf86e1af0c74e959b4237a8f999705de291
Diogo Verde <diogo.verde05@gmail.com>
2021-03-02 17:25:34 +0000

    initial steps and auth for nextjs and firebase app

---
+++ pages/admin/[slug].js
@@ -0,0 +1,7 @@
+export default function AdminPostEdit() {
+  return (
+    <main>
+      <h1>Admin Edit Post</h1>
+    </main>
+  );
+}
---
+++ pages/enter.js
@@ -0,0 +1,44 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function EnterPage() {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
+}
